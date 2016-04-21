00a9823b9638397ba4f6349f1f0f0994d7e535a1
bsaxen <benny.saxen@gmail.com>
2016-04-21 18:26:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ function getControlMessage($sid)
 function setClientStatus($sid,$name,$ip,$appid)
 //======================================= 
 {
-	echo("name=$name<br>ip=$ip<br>appid=$appid<br>")
+	echo("name=$name<br>ip=$ip<br>appid=$appid<br>");
 	if(!$name) $name = "noName";
 	if(!$ip) $ip = "noIp";
 	if(!$appid) $appid = "noAppid";
@@ -162,7 +162,7 @@ if(isset($_GET['mid']))
     	if($mid == SXN_BEACON)
     	{
     		setClientStatus(0,$name,$ip,$appid);
-    		echo("name=$name<br>ip=$ip<br>appid=$appid<br>")
+    		echo("name=$name<br>ip=$ip<br>appid=$appid<br>");
     		die();
     	}
     	$nsid = $_GET["nsid"];
