d4ada8917d9842ab4e42ff73b8f8f32fa24bfbdd
bsaxen <benny.saxen@gmail.com>
2016-10-10 06:08:10 +0200

    Update sxn_sidFunctions.php

--- sxn_sidFunctions.php
+++ sxn_sidFunctions.php
@@ -21,7 +21,7 @@ class controlSaxenHeater {
     $outdoorTemp_sid = 6;// 6
     $indoorTemp_sid  = 2;// 2
         
-    $time1 = 1200; // sec
+    $time1 = 120; // sec
     $time2 = 43200; //sec
         
     // Memeories
@@ -42,114 +42,71 @@ class controlSaxenHeater {
     $diff = strtotime($snow) - strtotime($prev);
     lib_log("CSH","Order latest: $snow [$prev] $diff($time1)\n");  
         
-    // Recall time and date for latest targetValue update
-    $prev = lib_recall($labelLatestTargetTime); 
-    if($prev == 'void')
-    {
-        $prev = $snow;
-        lib_remember($labelLatestTargetTime,$snow); 
-    }
-    $diff2 = strtotime($snow) - strtotime($prev);
-    lib_log("CSH","TargetValue latest update: [$prev] $diff2($time2)\n");      
-        
     //========================================================     
     //$waterIn     = lib_getLatestValue($waterIn_sid);
+    $waterIn    = lib_getLatestValue($waterIn_sid);
+    if($waterIn == SXN_NO_VALUE)
+    {
+        lib_log("CSH","No value for WaterIn\n");
+        //lib_log("CSH","-----End-----\n"); 
+        //return;
+    }
     $waterOut    = lib_getLatestValue($waterOut_sid);
     if($waterOut == SXN_NO_VALUE)
     {
         lib_log("CSH","No value for WaterOut\n");
-        lib_log("CSH","-----End-----\n"); 
+        //lib_log("CSH","-----End-----\n"); 
         //return;
     }
     $smokeTemp   = lib_getLatestValue($smokeTemp_sid);
     if($smokeTemp == SXN_NO_VALUE)
     {
         lib_log("CSH","No value for SmokeTemp\n");
-        lib_log("CSH","-----End-----\n"); 
+        //lib_log("CSH","-----End-----\n"); 
         //return;
     }
     $outdoorTemp = lib_getLatestValue($outdoorTemp_sid);
     if($outdoorTemp == SXN_NO_VALUE) 
     {
         lib_log("CSH","No value for OutdoorTemp\n"); 
-        lib_log("CSH","-----End-----\n"); 
+        //lib_log("CSH","-----End-----\n"); 
         //return;
     }    
     $indoorTemp  = lib_getLatestValue($indoorTemp_sid);
     if($outdoorTemp == SXN_NO_VALUE) 
     {
         lib_log("CSH","No value for IndoorTemp\n"); 
-        lib_log("CSH","-----End-----\n"); 
+        //lib_log("CSH","-----End-----\n"); 
         //return;
     }    
     //======================================================== 
-        
-    $targetValue = lib_recall($labelTargetValue);
-    if($targetValue == 'void')
-    {
-        $targetValue = 40;
-        lib_log("CSH","TargetValue=default($targetValue)\n");
-        lib_remember($labelTargetValue,$targetValue); 
-    }
-        
-        
-        
-    $delta = $targetValue - $outdoorTemp -  $waterOut;
- 
-    $logmsg = "TargetValue=".$targetValue."\n"; lib_log("CSH",$logmsg);
+              
     $logmsg = "Indoor=".$indoorTemp."\n";       lib_log("CSH",$logmsg);
     $logmsg = "WaterOut=".$waterOut."\n";       lib_log("CSH",$logmsg);
     $logmsg = "OutdoorTemp=".$outdoorTemp."\n"; lib_log("CSH",$logmsg);
     $logmsg = "SmokeTemp=".$smokeTemp."\n";     lib_log("CSH",$logmsg);
-    $logmsg = "Delta=".$delta."\n";             lib_log("CSH",$logmsg);
-        
-    //echo("diff=$diff $snow $prev<br>");
         
     lib_log("CSH","Action:");    
-    if($diff > $time1) // 20 minutes for order to effect the temperature  
+    if($diff > $time1) // 2 minutes for order to effect the temperature  
     {
     //echo("Algo: $delta<br>");
       lib_log("CSH","*");
       if($smokeTemp > 25.0) // Only control if Heater is ON
       {
         lib_log("CSH","!");
-        if($indoorTemp < 19.5) // Increase Heat
+        if($waterOutr < 26.0) // Increase Heat
         {
-           if($delta > 1.0)
-           {
               lib_log("CSH","stepper +");
-              $order = "NBC_STEPPER_CTRL 1 2 20";
+              $order = "NBC_STEPPER_CTRL 1 3 20";
               insertOrder($waterOut_sid,$order);
               lib_remember($labelLatestOrderTime,$snow); 
-           }
-           else if($diff2 > $time2) // 12 hours
-           {
-              $targetValue++;
-              if($targetValue > 50)$targetValue = 50;
-              lib_remember($labelTargetValue,$targetValue); 
-              lib_remember($labelLatestTargetTime,$snow); 
-              $logmsg = "\nIncreased TargetValue=$targetValue\n";
-              lib_log("CSH",$logmsg);
-           }
         }
-        if($indoorTemp > 20.5) // Decrease Heat
+        if($waterOut > 28.0) // Decrease Heat
         {
-           if($delta < -1.0)
-           {
               lib_log("CSH","stepper -");
-              $order = "NBC_STEPPER_CTRL 2 2 20";
+              $order = "NBC_STEPPER_CTRL 2 3 20";
               insertOrder($waterOut_sid,$order); 
               lib_remember($labelLatestOrderTime,$snow); 
-           } 
-           else if($diff2 > $time2) // 12 hours
-           {
-              $targetValue--;
-              if($targetValue < 10)$targetValue = 10;
-              lib_remember($labelTargetValue,$targetValue); 
-              lib_remember($labelLatestTargetTime,$snow); 
-              $logmsg = "\nDecreased TargetValue=$targetValue\n";
-              lib_log("CSH",$logmsg);
-           }
         }
       }
     }
